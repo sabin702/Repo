59a8e76d7d4498f86444d75e78e1d52b03bff1d1
Sabin <35765016+xplameboy@users.noreply.github.com>
2018-09-14 15:11:53 +0200

    HAHAHAHHAHAHAHAHAAHHA
    LOOOL

---
+++ src/bell/bell.java
@@ -0,0 +1,10 @@
+package bell;
+
+public class bell
+{
+   public static void main(String[] args) {
+      System.out.println("hallo");
+      System.out.println("Cry me a river");
+   }
+   
+}
---
+++ src/bell/something.java
@@ -0,0 +1,13 @@
+package bell;
+
+public class something
+{
+private int polute=1;
+
+public int getPolute(){
+   return polute;
+}
+public void haha() {
+   polute++;
+}
+}
